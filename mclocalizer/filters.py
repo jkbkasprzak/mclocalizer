fb22f4727c18878cbceece453a3036c47224e882
jkbkasprzak <73919276+jkbkasprzak@users.noreply.github.com>
2024-04-24 18:24:25 +0200

    Refine commit filters

--- mclocalizer/filters.py
+++ mclocalizer/filters.py
@@ -1,27 +1,65 @@
 import abc
 import re
 import pydriller as pyd
-from typing import Generator, Iterable
 
 
 class CommitFilter(abc.ABC):
-    def __init__(self, iterable: Iterable[pyd.Commit]):
-        self._iterable = iterable
+    """Abstract class for filtering out commits."""
 
     @abc.abstractmethod
-    def __iter__(self) -> Generator[pyd.Commit]:
+    def filter(self, commit: pyd.Commit) -> bool:
+        """Checks whether the commit passes.
+
+        :param commit: commit to be checked
+        :type commit: pyd.Commit
+        :returns: boolean, whether the commit has passed the filter
+        :rtype: bool
+        """
         pass
 
 
-class FixKeywordFilter(CommitFilter):
+class AlwaysTrueFilter(CommitFilter):
+    """Filter that returns True for all the commits."""
+
+    def filter(self, commit: pyd.Commit) -> bool:
+        return True
+
+
+class CommitFilterDecorator(CommitFilter):
+    """Abstract class that allows to chain multiple filters together.
+
+    New filters should inherit from this class and override _check function.
+    """
+
+    def __init__(self, prev_filter: CommitFilter = AlwaysTrueFilter()):
+        """Creates new filter
+
+        :param prev_filter: filter to check before this filter
+        :type prev_filter: CommitFilter
+        """
+        self.prev_filter = prev_filter
+
+    def filter(self, commit: pyd.Commit) -> bool:
+        return self.prev_filter.filter(commit) and self._check(commit)
+
+    @abc.abstractmethod
+    def _check(self, commit: pyd.Commit) -> bool:
+        """Checks whether the commit passes.
+
+        :param commit: commit to be checked
+        :type commit: pyd.Commit
+        :returns: boolean, whether the commit has passed the filter
+        :rtype: bool
+        """
+        pass
+
+
+class FixKeywordFilter(CommitFilterDecorator):
+    """Filter that returns True only for commits that mention fixing something in their message."""
+
     _pattern = re.compile(
         r"(\bfix\b)|(\bfixed\b)|(\bfixes\b)|(\bfixing\b)", re.IGNORECASE
     )
 
-    def is_fixing(self, commit: pyd.Commit) -> bool:
+    def _check(self, commit: pyd.Commit) -> bool:
         return re.search(FixKeywordFilter._pattern, commit.msg) is not None
-
-    def __iter__(self) -> Generator[pyd.Commit]:
-        for commit in self._iterable:
-            if self.is_fixing(commit):
-                yield commit
