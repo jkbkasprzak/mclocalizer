28f1ee2ab9c85f2940230a1196f1671819603c8f
jkbkasprzak <73919276+jkbkasprzak@users.noreply.github.com>
2024-05-04 16:43:12 +0200

    Simplify commit_filter module

--- mclocalizer/commit_filter.py
+++ mclocalizer/commit_filter.py
@@ -19,48 +19,12 @@ class CommitFilter(abc.ABC):
         pass
 
 
-class AlwaysTrueFilter(CommitFilter):
-    """Filter that returns True for all the commits."""
-
-    def filter(self, commit: pyd.Commit) -> bool:
-        return True
-
-
-class CommitFilterDecorator(CommitFilter):
-    """Abstract class that allows to chain multiple filters together.
-
-    New filters should inherit from this class and override _check function.
-    """
-
-    def __init__(self, prev_filter: CommitFilter = AlwaysTrueFilter()):
-        """Creates new filter
-
-        :param prev_filter: filter to check before this filter
-        :type prev_filter: CommitFilter
-        """
-        self.prev_filter = prev_filter
-
-    def filter(self, commit: pyd.Commit) -> bool:
-        return self.prev_filter.filter(commit) and self._check(commit)
-
-    @abc.abstractmethod
-    def _check(self, commit: pyd.Commit) -> bool:
-        """Checks whether the commit passes.
-
-        :param commit: commit to be checked
-        :type commit: pyd.Commit
-        :returns: boolean, whether the commit has passed the filter
-        :rtype: bool
-        """
-        pass
-
-
-class FixKeywordFilter(CommitFilterDecorator):
+class FixKeywordCommitFilter(CommitFilter):
     """Filter that returns True only for commits that mention fixing something in their message."""
 
     _pattern = re.compile(
         r"(\bfix\b)|(\bfixed\b)|(\bfixes\b)|(\bfixing\b)", re.IGNORECASE
     )
 
-    def _check(self, commit: pyd.Commit) -> bool:
-        return re.search(FixKeywordFilter._pattern, commit.msg) is not None
+    def filter(self, commit: pyd.Commit) -> bool:
+        return re.search(FixKeywordCommitFilter._pattern, commit.msg) is not None
